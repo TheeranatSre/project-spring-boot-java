17e9bdc442d85483ec0d6dfcabe65fab299bec08
TheeranatSre <78370042+TheeranatSre@users.noreply.github.com>
2023-12-08 15:38:13 +0700

    inti project java spring boot

---
+++ src/main/java/com/java/spring/projectspringbootjava/ProjectSpringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package com.java.spring.projectspringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringBootJavaApplication.class, args);
+	}
+
+}
